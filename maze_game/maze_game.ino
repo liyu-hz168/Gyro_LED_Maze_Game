3cd3162d7d9079a5d1faedcee069902a30d86248
MominHumayon <167389828+MominHumayon@users.noreply.github.com>
2025-12-03 19:00:35 -0500

    Adjust starting time value

--- maze_game/maze_game.ino
+++ maze_game/maze_game.ino
@@ -77,6 +77,7 @@ void setup(void) {
 
   // Draw the maze
   renderNewMaze(startingLv); // Copy data to matrix buffers
+  oldTime = millis();
 }
 
 // LOOP - RUNS REPEATEDLY AFTER SETUP --------------------------------------
